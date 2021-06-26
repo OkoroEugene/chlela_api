125fd5b30827c547e179763f26a6aa3458720745
Obinna Okoro <obinna.okoroeugene@gmail.com>
2021-06-26 13:15:24 +0100

    add file filter to help validate multer extensions

--- src/routes/file.ts
+++ src/routes/file.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import multer from 'multer';
 import { GetFiles, NewFile } from '../controllers/fileController';
+import {fileFilter} from '../utility/utility';
 
 const router = express.Router();
-const upload = multer({ dest: 'uploads/' });
-
+const upload = multer({ dest: 'uploads/', fileFilter });
+// upload.single('fileData')
 router.get('/', GetFiles)
-router.post('/', upload.single('fileData'), NewFile)
+router.post('/', NewFile)
 
 export {router as fileRouter};
---
+++ src/utility/utility.ts
@@ -0,0 +1,11 @@
+
+const fileFilter = function (req:any, file:any, cb:any) {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+
+export {fileFilter};
