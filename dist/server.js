8f97db51327ec890d4b53d39851ffa1461b41e70
Obinna Okoro <obinna.okoroeugene@gmail.com>
2021-06-28 13:30:57 +0100

    add env port

--- dist/server.js
+++ dist/server.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var body_parser_1 = require("body-parser");
 var routes_1 = require("./src/routes");
+var PORT = process.env.PORT || 5000;
 var app = express_1.default();
 app.use(body_parser_1.json());
 app.use(express_1.default.static("public"));
@@ -16,4 +17,4 @@ process.on('uncaughtException', function (err, origin) {
     console.error(err);
     console.log("Node NOT Exiting...");
 });
-app.listen(5000, function () { return console.log('Server is connected at 5000'); });
+app.listen(PORT, function () { return console.log('Server is connected at 5000'); });
